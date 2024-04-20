bc3098d17109efcd4968d59512f6a24aee429fca
LNcloud <161946390+LNcloud@users.noreply.github.com>
2024-04-20 12:08:15 +0530

    this script will create a EC2 instance

---
+++ create_instance.tf
@@ -0,0 +1,17 @@
+// Provider configuration for AWS
+provider "aws" {
+  region     = "us-east-2" // Specify the AWS region
+  
+}
+
+# Resource configuration for an EC2 instance
+resource "aws_instance"  {
+  ami           = "ami-0b8b44ec9a8f90422"            // AMI ID for the instance
+  instance_type = "t2.micro"    // Type of instance
+  key_name      = ""            // Key pair name (to be filled)
+
+  # Tags to identify resources
+  tags = {
+    Name = "demo-1"
+  }
+}
